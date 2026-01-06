57deb19ec98f64b76bed0c6b3b43967f466e8b54
venu9906 <venu990623@gmail.com>
2026-01-06 11:09:10 +0530

    simple if

---
+++ SimpleIf.java
@@ -0,0 +1,9 @@
+public class SimpleIf {
+    public static void main(String[] args) {
+        int age=20;
+
+        if(age>=18){
+            System.out.println("Eligible for vote");
+        }
+    }
+}
